05358108fe9a8c65fadb81b626ab5ff366e7a83e
Eric Azevedo Carvalho <snuknu@gmail.com>
2023-05-20 00:00:05 -0300

    Criação dos beans e das entidades do projeto.
    Ligação entre view (xhtml) e beans

---
+++ src/main/java/livraria/bean/LivroBean.java
@@ -0,0 +1,18 @@
+package livraria.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class LivroBean {
+
+	private Livro livro = new Livro();
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando um livro " + this.livro.getTitulo());
+	}
+
+}
