4e8d2febf4babd452e45e264adc08e5282711c31
Eric Azevedo Carvalho <snuknu@gmail.com>
2023-05-25 00:02:50 -0300

    Navegação e o ciclo de vida dos componentes JSF

---
+++ src/main/java/livraria/util/LogPhaseListener.java
@@ -0,0 +1,23 @@
+package livraria.util;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class LogPhaseListener implements PhaseListener{
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		System.out.println("FASE: " + event.getPhaseId().getName());
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.ANY_PHASE;
+	}
+
+}
